d630c97b8a9f9c56077f725859b1b3e84cfd562c
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-21 08:01:48 -0800

    updated file 2 for pep8

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     todo = requests.get(url + "todos", params={"userId": emp_id}).json()
     data = {str(emp_id): [{"task": task.get("title"),
                            "completed": task.get("completed"),
-                           "username": emp.get("username")} \
+                           "username": emp.get("username")}
                            for task in todo]}
     new_file = "{}.json".format(emp_id)
 
