57a215a0b8ce90db3a6754e395cdf538a64aebf2
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-21 05:53:45 -0800

    updated file 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,12 +6,12 @@ import sys
 
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
-    emp = requests.get(url + "users/{}".f(sys.arg[1])).json()
-    todo = requests.get(url + "todos", params={"userId": sys.arg[1]}).json()
-    for task in todo:
-        if task.get("completed"):
-            done.append(task.get("title"))
-    print("Employee {} is done with tasks({}/{}):".f(emp.get("name"),
-                                                     len(done), len(todo)))
-    for i in done:
-        print("\t {}".f(i))
+    emp_id = int(sys.argv[1])
+    emp = requests.get(url + "users/{}".format(emp_id)).json()
+    todo = requests.get(url + "todos", params={"userId": emp_id}).json()
+    done = [task for task in todo if task.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(emp.get("name"),
+                                                          len(done),
+                                                          len(todo)))
+    for task in done:
+        print("\t {}".format(task.get("title")))
